f298142930cf510d9c7dc419d4b5cf4459c16c8a
Vikas Solanki <solankivikas066@gmail.com>
2025-06-07 23:33:13 +0530

    Add Header skeleton

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import search from '../assets/images/icon-search.svg'
+import settings from '../assets/images/icon-settings.svg'
 
 const Header = () => {
+  
   return (
-    <div>
-      
+    <div className='header-container'>
+      <h1 className='page-title'>All Notes</h1>
+
+      <div className="header-right">
+        <div className="search-bar-container">
+          <img src={search} alt="" />
+          <input
+            className='search-bar'
+            placeholder='Search by title, content or tags...'
+            type="text"
+          />
+        </div>
+        <button className='setting-btn'>
+          <img src={settings} alt="Settings" />
+        </button>
+      </div>
     </div>
   )
 }
