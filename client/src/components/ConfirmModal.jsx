794f62b3da2d076da44364d66d10634344e50724
Vikas Solanki <solankivikas066@gmail.com>
2025-06-22 16:29:26 +0530

    Add Delete, Archive and Restore button function

--- client/src/components/ConfirmModal.jsx
+++ client/src/components/ConfirmModal.jsx
@@ -1,10 +1,37 @@
 import { useNote } from "../context/NoteContext"
 
-const ConfirmModal = ({}) => {
-  const {setAllNotes, selectedNote} = useNote()
-  const {setIsModalOpen, modalData} = useNote()
+const ConfirmModal = () => {
+  const {setAllNotes, selectedNote, setIsModalOpen, modalData} = useNote()
 
   const onConfirm = () => {
+    switch(modalData.title){
+      case 'Delete Note':
+        setAllNotes(prev => prev.filter(note => note.id !== selectedNote.id))
+        break;
+
+      case 'Archive Note':
+        setAllNotes(prev => prev.map(note => note.id === selectedNote.id ? ({...note, isArchived: true}) : note))
+        break;
+
+      case 'Restore Note':
+        setAllNotes(prev => prev.map(note => note.id === selectedNote.id ? ({...note, isArchived: false}) : note))
+        break;
+      
+      default:
+        break;
+    }
+    setIsModalOpen(false)
+  }
+
+  const getModalMsg = (title) => {
+    switch(title){
+      case 'Archive Note':
+        return "Are you sure you want to archive this note? You can find it in the Archived Notes section and restore it anytime."
+      case 'Restore Note':
+        return "Are you sure you want to restore this note? This note will be restored to All Notes section."
+      case 'Delete Note':
+        return "Are you sure you want to delete this note? This action cannot be undone."
+    }
   }
 
   return (
@@ -14,15 +41,7 @@ const ConfirmModal = ({}) => {
           <img src={modalData.icon} />
           <div className="modal-info">
             <h3>{modalData.title}</h3>
-              {modalData.title === 'Archive Note' &&
-                <p>Are you sure you want to archive this note? You can find it in the Archived Notes section and restore it anytime.</p>
-              }
-              {modalData.title === 'Restore Note' &&
-                <p>Are you sure you want to restore this note? This note will be restored to All Notes section.</p>
-              }
-              {modalData.title === 'Delete Note' &&
-                <p>Are you sure you want to delete this note? This action cannot be undone.</p>
-              }
+            <p>{getModalMsg(modalData.title)}</p>
           </div>
         </div>
         <div className="modal-btn-wrapper">
