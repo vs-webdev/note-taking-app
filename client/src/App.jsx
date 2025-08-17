2b0ed7e8b50a428863d2e5f79728ca49823fa808
Vikas Solanki <solankivikas066@gmail.com>
2025-08-17 22:38:10 +0530

    Implement browser router and private routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css'
 import AllNotes from './pages/AllNotes'
+import AuthPage from './pages/AuthPage'
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import { useAuth } from './context/AuthContext'
+
+const PrivateRoute = ({children}) => {
+  const {isAuthenticated} = useAuth()
+  return isAuthenticated ? children : <Navigate to="/auth" />
+}
 
 function App() {
-
+  const {isAuthenticated} = useAuth()
+  
   return (
     <>
-      <AllNotes />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<PrivateRoute>
+              <AllNotes />
+            </PrivateRoute> }
+          />
+
+          <Route path='/auth' element={isAuthenticated ? <Navigate to="/" /> : <AuthPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
